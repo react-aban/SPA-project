eba0a8dc716899b47f410d45c0e6cf239c7a13c3
Paria Razavi <paria.razavi29@gmail.com>
2024-10-25 22:50:26 +0330

    feature/add extra

--- src/components/users/List.tsx
+++ src/components/users/List.tsx
@@ -1,37 +1,49 @@
 import React from "react";
+import { Link } from "umi";
 import { Card, List } from "antd";
 
+interface User {
+  id: number; 
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
 interface UserListProps {
-  users: { name: string; username: string; email: string; phone: string; website: string }[];
+  users: User[];
   loading: boolean;
 }
 
 const UserList: React.FC<UserListProps> = ({ users, loading }) => {
   return (
-    <>
-      <List
-        grid={{
-          gutter: 16,
-          xs: 1,
-          sm: 2,
-          md: 2,
-          lg: 3,
-          xl: 3,
-          xxl: 4,
-        }}
-        dataSource={users}
-        renderItem={(user) => (
-          <List.Item>
-            <Card loading={loading} title={user.name}>
-              <p>Username: {user.username}</p>
-              <p>Email: {user.email}</p>
-              <p>Phone: {user.phone}</p>
-              <p>Website: {user.website}</p>
-            </Card>
-          </List.Item>
-        )}
-      />
-    </>
+    <List
+      grid={{
+        gutter: 16,
+        xs: 1,
+        sm: 2,
+        md: 2,
+        lg: 3,
+        xl: 3,
+        xxl: 4,
+      }}
+      dataSource={users}
+      renderItem={(user) => (
+        <List.Item>
+          <Card 
+            loading={loading} 
+            title={user.name} 
+            extra={<Link to={`/users/${user.id}`}>Details</Link>} 
+          >
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+          </Card>
+        </List.Item>
+      )}
+    />
   );
 };
 
