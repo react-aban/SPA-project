d20c3e4083033a6056711c3fbe02175e06961ab1
Paria Razavi <paria.razavi29@gmail.com>
2024-10-27 21:33:16 +0330

    refactor/isolate user api

--- src/pages/users/List.tsx
+++ src/pages/users/List.tsx
@@ -1,36 +1,41 @@
 import React from "react";
 import { Link } from "umi";
-import { Card, List } from "antd";
+import { Card, List, Spin } from "antd"; 
 import { UserListProps } from "@/types/UserTypes";
 
 const UserList: React.FC<UserListProps> = ({ users, loading }) => {
   return (
-    <List
-      grid={{
-        gutter: 16,
-        xs: 1,
-        sm: 2,
-        md: 2,
-        lg: 3,
-        xl: 3,
-        xxl: 4,
-      }}
-      dataSource={users}
-      renderItem={(user) => (
-        <List.Item>
-          <Card
-            loading={loading}
-            title={user.name}
-            extra={<Link to={`/users/${user.id}`}>Details</Link>}
-          >
-            <p>Username: {user.username}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone: {user.phone}</p>
-            <p>Website: {user.website}</p>
-          </Card>
-        </List.Item>
+    <>
+      {loading ? (
+        <Spin size="large" />
+      ) : (
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 2,
+            lg: 3,
+            xl: 3,
+            xxl: 4,
+          }}
+          dataSource={users}
+          renderItem={(user) => (
+            <List.Item key={user.id}> 
+              <Card
+                title={user.name}
+                extra={<Link to={`/users/${user.id}`}>Details</Link>}
+              >
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <p>Phone: {user.phone}</p>
+                <p>Website: {user.website}</p>
+              </Card>
+            </List.Item>
+          )}
+        />
       )}
-    />
+    </>
   );
 };
 
